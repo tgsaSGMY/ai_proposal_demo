c20b95b5adef0c0314cae9c1ca7bdbaaa8a3e5b7
tgsaSGMY <smtt@gsatw.com>
2025-11-07 10:08:57 +0800

    try to patch supabase log out

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -287,14 +287,6 @@ async function checkAuth() {
 // 登出功能
 async function handleLogout() {
   try {
-    const { data } = await supabase.auth.getSession();
-    console.log(data.session);
-    if (!data.session) {
-      console.warn("No active session, just redirecting.");
-      await useRouter().push("/login");
-      return;
-    }
-
     const { error } = await supabase.auth.signOut();
     if (error) throw error;
     isAuthenticated.value = false;
